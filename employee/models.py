83d03dae719b48e37aa3e7733b54350768498f50
Egor <Yegor.lob@yandex.ru>
2021-08-23 22:45:13 +0300

    branch#3

--- employee/models.py
+++ employee/models.py
@@ -1,18 +1,34 @@
 from django.db import models
 
 
-class EmployeeModel(models.Model):
-    device = models.CharField(max_length=50)
-    configuration = models.URLField(max_length=100, blank=True)
-    price = models.CharField(max_length=50)
-    paid_by = models.CharField(max_length=50)
-    used_by = models.CharField(max_length=50)
+class Employee(models.Model):
     image = models.ImageField(verbose_name='image', blank=True,
                               upload_to='employee/static/images')
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    age = models.PositiveSmallIntegerField()
     team = models.CharField(max_length=50)
-    comment = models.CharField(max_length=250)
-    item_number = models.PositiveSmallIntegerField()
-    empl_start_date = models.DateField(auto_now_add=True)
+    start_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'Employees'
+        verbose_name_plural = 'Employees'
 
     def __str__(self):
-        return self.used_by
+        return f'{self.firstname} {self.lastname}'
+
+
+class Device(models.Model):
+    device = models.CharField(max_length=100)
+    configuration = models.URLField(max_length=250, blank=True)
+    price = models.PositiveSmallIntegerField()
+    paid_by = models.CharField(max_length=50)
+    used_by = models.ForeignKey(Employee, verbose_name='Used by', on_delete=models.SET_NULL, null=True, blank=True)
+    item_number = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.device
+
+    class Meta:
+        db_table = 'Devices'
+        verbose_name_plural = 'Devices'
--- employee/urls.py
+++ employee/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from employee.views import EmployeePage, EmployeeViewSet
+from employee.views import EmployeesPage, DevicesPage, EmployeesViewSet, DevicesViewSet
 
 router = DefaultRouter()
-router.register('api', EmployeeViewSet)
+router.register('api/Employees', EmployeesViewSet)
+router.register('api/Devices', DevicesViewSet)
 
 urlpatterns = [
-    path('', EmployeePage, name='employee'),
+    path('employees', EmployeesPage, name='employee'),
+    path('devices', DevicesPage, name='device'),
 ]
 
 urlpatterns += router.urls
